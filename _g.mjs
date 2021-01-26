f270f7b2495c95144a8d5b2e69fd43f920a46b08
taoqf <tao_qiufeng@126.com>
2021-01-26 12:18:38 +0800

    gitee

--- _g.mjs
+++ _g.mjs
@@ -9,18 +9,24 @@ async function main() {
 	const root = './steps';
 	console.time('mm');
 	const files = await readdir(root);
-	const indexes = await Promise.all(files.map(async (file, i) => {
+	const indexes = await Promise.all(files.filter((file) => {
+		return file.endsWith('.md');
+	}).map(async (file, i) => {
 		const path = join(root, file);
 		const txt = await readFile(path, 'utf8');
 		const title = /.*#\s(.*)\n/.exec(txt)[1];
 		const idx = (i + 1).toString().padStart(2, '0');
-		return `- [第${idx}章 ${title}](${path.replace('.md', '')})`;
+		return `- [第${idx}章 ${title}](./${path.replace('.md', '')})`;
 	}));
 	const content = `# 蛮蛮工作室
 
 全民编程
 
 ${indexes.join('\n')}
+
+## 您的支持是我继续的动力
+
+![打赏](./images/dashang.jpg)
 `;
 	await writeFile('./index.md', content, 'utf8');
 	console.timeEnd('mm');
